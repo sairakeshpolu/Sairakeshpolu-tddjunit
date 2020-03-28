4dc14a8f800446cfedcfff6991dc97753bc94db8
sairakeshpolu <60548825+sairakeshpolu@users.noreply.github.com>
2020-03-28 11:00:04 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package epam_tdd.TDD_JU;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ConsecutiveAs.java
@@ -0,0 +1,14 @@
+package epam_tdd.TDD_JU;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ConsecutiveAs {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
